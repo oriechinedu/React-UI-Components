83537dad5d166a2bcc94f9a9acc9ebe6ea6a3956
Orie Chinedu <oriechinedu@gmail.com>
2019-05-27 16:24:49 +0100

    Use unicode for dot

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 function headerTitle() {
   return (
-    <h2 className="header-title"> Lambda School<span>@LambdaSchool <span className="dot">.</span> 26 jan</span></h2>
+    <h4 className="header-title">Lambda School <span> @LambdaSchool <span className="dot">&#8226;</span> 26 jan</span></h4>
   )
 }
 
