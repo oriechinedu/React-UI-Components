64e4a80dea681106bc1cc6bc57141c3070a2e36e
Orie Chinedu <oriechinedu@gmail.com>
2019-05-27 16:04:38 +0100

    Make Page responsive

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ImageThumbnail from './ImageThumbnail'
-import HeaderTitle from './HeaderTitle'
 import HeaderContent from './HeaderContent'
 import './Header.css';
 
